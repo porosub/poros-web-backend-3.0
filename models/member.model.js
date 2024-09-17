1b7c06d99e67b8caa367a7b30c4000eb62d740d8
irfnhanif <14.irfanh@gmail.com>
2024-09-17 15:42:53 +0700

    Add Member model with database configuration and synchronization

---
+++ models/member.model.js
@@ -0,0 +1,47 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const Blog = sequelize.define(
+  "Blog",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "members",
+    timestamps: true,
+  }
+);
+
+Blog.sync()
+  .then(() => {})
+  .catch((error) => {
+    console.error("Failed to sync Blog model:", error);
+  });
+
+export default Blog;
