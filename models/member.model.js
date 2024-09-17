aa1918c6d2f1b1aeda27b05615416c4ac2a67098
irfnhanif <14.irfanh@gmail.com>
2024-09-17 16:04:51 +0700

    Add Member model with database configuration and synchronization

---
+++ models/member.model.js
@@ -0,0 +1,53 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+const Member = sequelize.define(
+  "Member",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    position: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    division: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imageURL: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true,
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "members",
+    timestamps: true,
+  }
+);
+
+Member.sync()
+  .then(() => {})
+  .catch((error) => {
+    console.error("Failed to sync Member model:", error);
+  });
+
+export default Member;
