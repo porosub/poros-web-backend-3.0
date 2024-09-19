364f0a78da31289c5f4a949d6774072aca08782c
irfnhanif <14.irfanh@gmail.com>
2024-09-19 09:01:18 +0700

    Add authentication routes to index router

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { blogRouter } from "./blog.routes.js";
 import { memberRouter } from "./member.routes.js";
+import { authRouter } from "./auth.routes.js";
 
 const indexRouter = Router();
 
 indexRouter.use("/blogs", blogRouter);
 indexRouter.use("/members", memberRouter);
+indexRouter.use("/auth", authRouter);
+
+export default indexRouter;
