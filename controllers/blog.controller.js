a4caee7e714e004df6c7e33ce924c5554d764974
irfnhanif <14.irfanh@gmail.com>
2024-09-19 09:19:56 +0700

    Add blog controller with CRUD operations

---
+++ controllers/blog.controller.js
@@ -0,0 +1,11 @@
+import Blog from "../models/blog.model.js";
+
+export const getAllBlogs = (req, res) => {};
+
+export const createBlog = (req, res) => {};
+
+export const getBlogById = (req, res) => {};
+
+export const updateBlogById = (req, res) => {};
+
+export const deleteBlogById = (req, res) => {};
