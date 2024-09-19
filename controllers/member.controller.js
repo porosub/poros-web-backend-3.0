201d3786bb009ae2ef1e91654855939c800c1d13
irfnhanif <14.irfanh@gmail.com>
2024-09-19 09:20:03 +0700

    Add member controller with CRUD operations

---
+++ controllers/member.controller.js
@@ -0,0 +1,11 @@
+import Member from "../models/member.model.js";
+
+export const getAllMembers = (req, res) => {};
+
+export const createMember = (req, res) => {};
+
+export const getMemberById = (req, res) => {};
+
+export const updateMemberById = (req, res) => {};
+
+export const deleteMemberById = (req, res) => {};
