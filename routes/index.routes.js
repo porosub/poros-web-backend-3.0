4ea74b553721d2995ba6f200b89ffc4040ef8048
irfnhanif <14.irfanh@gmail.com>
2024-09-24 16:35:36 +0700

    Update import paths for sequelize in admin and member models

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { blogRouter } from "./blog.routes.js";
-import { memberRouter } from "./member.routes.js";
-import { authRouter } from "./auth.routes.js";
+import blogRouter from "./blog.routes.js";
+import memberRouter from "./member.routes.js";
+import authRouter  from "./auth.routes.js";
 
 const indexRouter = Router();
 
