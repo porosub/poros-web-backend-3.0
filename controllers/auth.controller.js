f425d26fee559546d547251727bd1b3cf37514e4
irfnhanif <14.irfanh@gmail.com>
2024-09-24 19:01:36 +0700

    Add Admin model import to auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
-export const signup = async (req, res) => {
+import Admin from "../models/admin.model.js";
 
-}
+export const signup = async (req, res) => {};
 
 export const signin = async (req, res) => {};
