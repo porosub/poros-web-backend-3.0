be91833dced5377360f19cfd131e7fc752520420
Dani Adrian <daniadrian1311@gmail.com>
2024-09-25 23:13:20 +0700

    new feature: get all blogs

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,6 +1,18 @@
 import Blog from "../models/blog.model.js";
 
-export const getAllBlogs = (req, res) => {};
+export const getAllBlogs = async (req, res) => {
+  try {
+    // Query the database for all blog entries
+    const blogs = await Blog.findAll();
+
+    // Send the blogs back in the response
+    return res.status(200).json(blogs);
+  } catch (error) {
+    // Handle errors and send an appropriate response
+    console.error("Error fetching blogs:", error);
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const createBlog = (req, res) => {};
 
