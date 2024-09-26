894253e374aa254e079cefdca7a716e2928c6cfd
irfnhanif <14.irfanh@gmail.com>
2024-09-26 19:02:38 +0700

    Refactor variable names in blogpost.controller.js for clarity

--- controllers/blogpost.controller.js
+++ controllers/blogpost.controller.js
@@ -2,10 +2,10 @@ import BlogPost from "../models/blogpost.model.js";
 
 export const getAllBlogPosts = async (req, res) => {
   try {
-    const blogposts = await BlogPost.findAll();
-    return res.status(200).json(blogposts);
+    const blogPosts = await BlogPost.findAll();
+    return res.status(200).json(blogPosts);
   } catch (error) {
-    console.error("Error fetching blogposts:", error);
+    console.error("Error fetching blog posts:", error);
     return res.status(500).json({ message: error.message });
   }
 };
@@ -21,18 +21,18 @@ export const createBlogPost = async (req, res) => {
 
     return res.status(201).json(newBlogPost);
   } catch (error) {
-    console.error("Error creating blogpost:", error);
+    console.error("Error creating blog post:", error);
     return res.status(500).json({ message: error.message });
   }
 };
 
 export const getBlogPostById = async (req, res) => {
   try {
-    const blogposts = await BlogPost.findByPk(req.params.id);
+    const blogPost = await BlogPost.findByPk(req.params.id);
 
-    return res.status(200).json(blogposts);
+    return res.status(200).json(blogPost);
   } catch (error) {
-    console.error("Error fetching blogposts:", error);
+    console.error("Error fetching blog post:", error);
     return res.status(500).json({ message: error.message });
   }
 };
@@ -41,17 +41,17 @@ export const updateBlogPostById = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const blogpost = await BlogPost.findByPk(id);
+    const blogPost = await BlogPost.findByPk(id);
 
-    if (!blogpost) {
+    if (!blogPost) {
       return res.status(404).json({ message: "BlogPost not found" });
     }
 
-    await blogpost.update(req.body);
+    await blogPost.update(req.body);
 
-    return res.status(200).json(blogpost);
+    return res.status(200).json(blogPost);
   } catch (error) {
-    console.error("Error updating blogpost:", error);
+    console.error("Error updating blog post:", error);
     return res.status(500).json({ message: error.message });
   }
 };
@@ -60,17 +60,17 @@ export const deleteBlogPostById = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const blogpost = await BlogPost.findByPk(id);
+    const blogPost = await BlogPost.findByPk(id);
 
-    if (!blogpost) {
+    if (!blogPost) {
       return res.status(404).json({ message: "BlogPost not found" });
     }
 
-    await blogpost.destroy();
+    await blogPost.destroy();
 
     return res.status(200).json({ message: "BlogPost deleted successfully" });
   } catch (error) {
-    console.error("Error deleting blogpost:", error);
+    console.error("Error deleting blog post:", error);
     return res.status(500).json({ message: error.message });
   }
 };
