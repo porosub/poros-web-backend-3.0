f6eb2af435677966fd0ef350385ab765652c75c3
irfnhanif <101242947+irfnhanif@users.noreply.github.com>
2024-09-26 11:07:56 +0700

    Update blog.controller.js

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -2,13 +2,9 @@ import Blog from "../models/blog.model.js";
 
 export const getAllBlogs = async (req, res) => {
   try {
-    // Query the database for all blog entries
     const blogs = await Blog.findAll();
-
-    // Send the blogs back in the response
     return res.status(200).json(blogs);
   } catch (error) {
-    // Handle errors and send an appropriate response
     console.error("Error fetching blogs:", error);
     return res.status(500).json({ message: error.message });
   }
