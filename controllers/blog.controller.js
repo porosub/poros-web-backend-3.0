ba0695138ad68969a83d6ae0c262cd3172af3d86
Dani Adrian <daniadrian1311@gmail.com>
2024-09-27 00:52:01 +0700

    update pagination

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,10 +1,22 @@
 import Blog from "../models/blog.model.js";
 
 export const getAllBlogs = async (req, res) => {
-  try {
-    const blogs = await Blog.findAll();
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const offset = (page - 1) * limit;
 
-    return res.status(200).json(blogs);
+  try {
+    const { count, rows: blogs } = await Blog.findAndCountAll({
+      offset,
+      limit,
+    });
+
+    return res.status(200).json({
+      totalItems: count,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      blogs,
+    });
   } catch (error) {
     console.error("Error fetching blogs:", error);
     return res.status(500).json({ message: error.message });
