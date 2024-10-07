acd3f9191acf63ee5bae01b58ee3173723eee35c
irfnhanif <14.irfanh@gmail.com>
2024-10-07 12:57:55 +0700

    Refactor member.controller.js to import Joi for input validation and add code to updateMember

--- controllers/member.controller.js
+++ controllers/member.controller.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import Joi from "joi";
 import "dotenv/config";
 import { Op } from "@sequelize/core";
 import Member from "../models/member.model.js";
@@ -37,7 +38,7 @@ export const getAllMembers = async (req, res) => {
       Member.findAll({ where: { group: "bpi" } }),
       Member.findAll({ where: { group: "bph" } }),
       Member.findAndCountAll({
-        where: { group: { [Op.is]: null } },
+        where: { group: { [Op.is]: "-" } },
         order: [["division"], ["name"]],
         offset,
         limit,
@@ -60,6 +61,11 @@ export const getAllMembers = async (req, res) => {
 
 export const createMember = async (req, res) => {
   try {
+    const { isValid, error } = validateMember(req.body);
+    if (!isValid) {
+      return res.status(400).json({ message: error });
+    }
+
     const { name, position, division, group, image } = req.body;
 
     const { isSuccessful, imageURL, error } = processImage(req.body);
@@ -96,7 +102,38 @@ export const getMemberById = async (req, res) => {
   }
 };
 
-export const updateMemberById = (req, res) => {};
+export const updateMemberById = async (req, res) => {
+  try {
+    const member = await Member.findByPk(req.params.id);
+    if (!member) {
+      return res.status(404).json({ message: "Member not found" });
+    }
+
+    const { isValid, error } = validateMember(req.body);
+    if (!isValid) {
+      return res.status(400).json({ message: error });
+    }
+
+    const { isSuccessful, imageURL, error } = processImage(req.body);
+    if (!isSuccessful && error !== "File already exist") {
+      return res.status(400).json({ message: error });
+    }
+
+    member.name = req.body.name;
+    member.position = req.body.position;
+    member.division = req.body.division;
+    member.group = req.body.group;
+    if (!error) {
+      member.imageURL = imageURL;
+    }
+
+    await member.save();
+    return res.status(200).json(member);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const deleteMemberById = async (req, res) => {
   try {
@@ -115,6 +152,33 @@ export const deleteMemberById = async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: error.message });
+  }
+};
+
+const validateMember = (memberInput) => {
+  const memberValidationSchema = Joi.object({
+    name: Joi.string().required(),
+    position: Joi.string().required(),
+    division: Joi.string()
+      .valid("Back-end", "Front-end", "Cybersecurity")
+      .required(),
+    group: Joi.string().valid("bpi", "bph", "-").required(),
+    image: Joi.string()
+      .pattern(/^data:image\/[a-zA-Z]+;base64,[A-Za-z0-9+/=]+$/)
+      .required(),
+  });
+
+  const { error } = memberValidationSchema.validate(blogPostInput, {
+    abortEarly: false,
+  });
+
+  if (error) {
+    return {
+      isValid: false,
+      error: error.details.map((detail) => detail.message).join(", "),
+    };
+  } else {
+    return { isValid: true, error: null };
   }
 };
 
