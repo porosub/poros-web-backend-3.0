050fea6efa441534e2962a1e5aa8707db47bfa74
irfnhanif <14.irfanh@gmail.com>
2024-10-08 18:20:04 +0700

    Refactor registration key handling in signup function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,13 +5,13 @@ import Joi from "joi";
 import Admin from "../models/admin.model.js";
 
 export const signup = async (req, res) => {
-  const { registrationKeys, ...adminData } = req.body;
+  const { registrationKey: registrationKey, ...adminData } = req.body;
 
   if (
-    !registrationKeys ||
-    registrationKeys !== process.env.REGISTRATION_SECRET_KEY
+    !registrationKey ||
+    registrationKey !== process.env.REGISTRATION_SECRET_KEY
   ) {
-    return res.statusCode(403).json({ message: "Forbidden" });
+    return res.status(403).json({ message: "Forbidden" });
   }
 
   const registerAdminValidationSchema = Joi.object({
@@ -23,30 +23,30 @@ export const signup = async (req, res) => {
   const { error } = registerAdminValidationSchema.validate(adminData);
 
   if (error) {
-    return res.statusCode(400).json({
+    return res.status(400).json({
       message: error.details.map((detail) => detail.message).join(", "),
     });
   }
 
-  const existingAdmin = Admin.findOne({
+  const existingAdmin = await Admin.findOne({
     where: {
       username: adminData.username,
     },
   });
   if (existingAdmin) {
-    return res.statusCode(409).json({ message: "Admin already exist" });
+    return res.status(409).json({ message: "Admin already exist" });
   }
 
   const salt = await bcrypt.genSalt(12);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const hashedPassword = await bcrypt.hash(adminData.password, salt);
 
   await Admin.create({
     username: adminData.username,
     password: hashedPassword,
-    role: registrationKeys,
+    role: adminData.role,
   });
 
-  return res.statusCode(201).json({ message: "Admin registered successfully" });
+  return res.status(201).json({ message: "Admin registered successfully" });
 };
 
 export const signin = (req, res) => {
