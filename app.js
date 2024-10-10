c2a22412e8c2b5943bd652bc2ab98112a865433e
irfnhanif <14.irfanh@gmail.com>
2024-10-10 08:32:16 +0700

    Refactor CORS configuration in app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import express from "express";
+import cors from 'cors';
 import "dotenv/config";
 import indexRouter from "./routes/index.routes.js";
 
 const app = express();
 
 app.use(express.json({ limit: "20mb" }));
-
+app.use(cors({
+  origin: process.env.CLIENT_ADDRESS
+}))
 app.use("/", indexRouter);
 
-
-// Start the server
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running on ${process.env.BASE_URL}:${process.env.APP_PORT}`);
 });
