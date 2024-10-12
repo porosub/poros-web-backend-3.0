e7381d9f5efdc3a302357f47ed6528d3db1ad789
irfnhanif <14.irfanh@gmail.com>
2024-10-12 10:56:26 +0700

    Refactor index routes to disable blog post routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,7 +6,7 @@ import authRouter from "./auth.routes.js";
 const indexRouter = express.Router();
 
 indexRouter.use("/auth", authRouter);
-indexRouter.use("/blogposts", blogPostRouter);
+// indexRouter.use("/blogposts", blogPostRouter);
 indexRouter.use("/members", memberRouter);
 indexRouter.use("/images", express.static(process.env.IMAGE_STORAGE_LOCATION));
 
