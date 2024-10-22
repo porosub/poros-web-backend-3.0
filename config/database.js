4a9e5d58a7fbb37641f770dddb372f503198b29a
RushingAlien <rushing27alien@gmail.com>
2024-10-22 15:32:45 +0700

    add missing .js extension

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 import fs from "fs"
-import { getSecret } from './secrets';
+import { getSecret } from './secrets.js';
 
 
 const sequelize = new Sequelize(
