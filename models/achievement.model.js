d8b03bec4f4640b3edb42d1cd3a7e986a82d8479
irfnhanif <14.irfanh@gmail.com>
2024-10-30 08:09:48 +0700

    Add Achievement model for managing achievements

---
+++ models/achievement.model.js
@@ -0,0 +1,54 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const Achievement = sequelize.define(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    competitionName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "competition_name",
+    },
+    teamName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "team_name",
+    },
+    imageFileName: {
+      type: DataTypes.STRING,
+      field: "image_file_name",
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+      field: "created_at",
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+      field: "updated_at",
+    },
+  },
+  {
+    tableName: "achievements",
+    timestamps: true,
+  }
+);
+
+Achievement.sync()
+  .then(() => {})
+  .catch((error) => {
+    console.error("Failed to sync Achievement model:", error);
+  });
+
+export default Achievement;
