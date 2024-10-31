0cbf97ec4f51bfd58f6389ec288ac2422e66a065
irfnhanif <14.irfanh@gmail.com>
2024-10-31 10:52:07 +0700

    Add achievement controller with CRUD operations

---
+++ controllers/achievement.controller.js
@@ -0,0 +1,9 @@
+export const createAchievement = (req, res) => {};
+
+export const getAllAchievements = (req, res) => {};
+
+export const getAchievementById = (req, res) => {};
+
+export const updateAchievementById = (req, res) => {};
+
+export const deleteAchievementById = (req, res) => {};
