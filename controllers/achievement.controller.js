68376c8939f9fcdd68e931247c9b61cbd1ff46da
irfnhanif <14.irfanh@gmail.com>
2024-10-31 10:59:23 +0700

    Add achievement model import and validation function in achievement controller

--- controllers/achievement.controller.js
+++ controllers/achievement.controller.js
@@ -1,3 +1,5 @@
+import Achievement from "../models/achievement.model.js";
+
 export const createAchievement = (req, res) => {};
 
 export const getAllAchievements = (req, res) => {};
@@ -7,3 +9,27 @@ export const getAchievementById = (req, res) => {};
 export const updateAchievementById = (req, res) => {};
 
 export const deleteAchievementById = (req, res) => {};
+
+const validateAchievement = (achievementInput) => {
+  const achievementValidationSchema = Joi.object({
+    title: Joi.string().required(),
+    competitionName: Joi.string().required(),
+    teamName: Joi.string().required(),
+    image: Joi.string().pattern(
+      /^data:image\/[a-zA-Z]+;base64,[A-Za-z0-9+/=]+$/
+    ),
+  });
+
+  const { error } = achievementValidationSchema.validate(achievementInput, {
+    abortEarly: false,
+  });
+
+  if (error) {
+    return {
+      isValid: false,
+      error: error.details.map((detail) => detail.message).join(", "),
+    };
+  } else {
+    return { isValid: true, error: null };
+  }
+};
