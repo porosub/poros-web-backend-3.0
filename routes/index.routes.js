a04a093f6410c9b5fcfdee77306a9926fcd0d45c
irfnhanif <14.irfanh@gmail.com>
2024-11-09 13:26:10 +0700

    Add achievement router to main routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,12 +3,14 @@ import cacheImage from "../middlewares/cache-image.middleware.js";
 import blogPostRouter from "./blogpost.routes.js";
 import memberRouter from "./member.routes.js";
 import authRouter from "./auth.routes.js";
+import achievementRouter from "./achievement.routes.js";
 
 const indexRouter = express.Router();
 
 indexRouter.use("/auth", authRouter);
 // indexRouter.use("/blogposts", blogPostRouter);
 indexRouter.use("/members", memberRouter);
+indexRouter.use("/achievements", achievementRouter);
 indexRouter.use(
   "/images",
   cacheImage,
