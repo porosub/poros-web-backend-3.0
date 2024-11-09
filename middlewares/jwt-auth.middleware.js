e0a2b87dd2589fa166c3b1c56bdc96d4f1c0ef5a
irfnhanif <14.irfanh@gmail.com>
2024-11-09 15:14:40 +0700

    Refactor JWT authentication middleware to ensure early returns on error responses

--- middlewares/jwt-auth.middleware.js
+++ middlewares/jwt-auth.middleware.js
@@ -3,10 +3,10 @@ import passport from "../config/passport.js";
 const jwtAuthMiddleware = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, admin, info) => {
     if (err) {
-      res.status(500).json({ message: "An error occurred", error: err });
+      return res.status(500).json({ message: "An error occurred", error: err });
     }
     if (!admin) {
-      res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
     req.admin = admin;
     next();
